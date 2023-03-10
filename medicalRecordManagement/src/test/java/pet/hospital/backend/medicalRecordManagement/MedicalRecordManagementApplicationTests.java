31b0ccfda3f86b8367fad65fd0e5997efc2548ed
pikapikapi <pikapikapi_kaori@icloud.com>
2023-03-10 10:50:28 +0800

    config: config project

---
+++ medicalRecordManagement/src/test/java/pet/hospital/backend/medicalRecordManagement/MedicalRecordManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.hospital.backend.medicalRecordManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalRecordManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
