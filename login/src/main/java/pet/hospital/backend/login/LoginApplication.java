db44a11133646b4bf0da37b63495e36473383087
pikapikapikaori <pikapikapi_kaori@icloud.com>
2023-03-15 22:43:51 +0800

    config: Remove swagger module

--- login/src/main/java/pet/hospital/backend/login/LoginApplication.java
+++ login/src/main/java/pet/hospital/backend/login/LoginApplication.java
@@ -1,11 +1,22 @@
+/*
+ * @Author: pikapikapikaori pikapikapi_kaori@icloud.com
+ * @Date: 2023-03-15 10:50:57
+ * @LastEditors: pikapikapikaori pikapikapi_kaori@icloud.com
+ * @LastEditTime: 2023-03-15 22:24:48
+ * @FilePath: /virtualPetHospital-backend/login/src/main/java/pet/hospital/backend/login/LoginApplication.java
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 package pet.hospital.backend.login;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "鉴权模块", version = "0.0.1-SNAPSHOT", description = "虚拟宠物医院API文档-鉴权模块"))
 public class LoginApplication {
 
     public static void main(String[] args) {
