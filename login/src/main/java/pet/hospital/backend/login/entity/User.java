f83cfb5ea0b7028fa8d26818f44e8ea872bb2fc4
pikapikapikaori <pikapikapi_kaori@icloud.com>
2023-03-15 12:08:04 +0800

    feat: Add login database

--- login/src/main/java/pet/hospital/backend/login/entity/User.java
+++ login/src/main/java/pet/hospital/backend/login/entity/User.java
@@ -1,6 +1,10 @@
 package pet.hospital.backend.login.entity;
 
-import javax.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,11 +12,18 @@ import lombok.Setter;
 @Setter
 @Entity
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     public int user_id;
 
+    @Column
     public String user_name;
 
+    @Column
     public String password;
 
+    @Column
     public int authority;
 }
