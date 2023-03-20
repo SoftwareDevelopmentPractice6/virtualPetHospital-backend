d7db58aeab446966349529d80c61aafc6acd451b
pikapikapikaori <pikapikapi_kaori@icloud.com>
2023-03-20 15:29:44 +0800

    fix: 修正表名映射问题

--- exam/src/main/java/pet/hospital/backend/exam/entity/ExamSession.java
+++ exam/src/main/java/pet/hospital/backend/exam/entity/ExamSession.java
@@ -8,6 +8,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
 import java.util.Date;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,6 +17,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "EXAMSESSION")
 public class ExamSession {
 
     @Id
--- exam/src/main/java/pet/hospital/backend/exam/entity/StudentResult.java
+++ exam/src/main/java/pet/hospital/backend/exam/entity/StudentResult.java
@@ -15,12 +15,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@Table(name = "STUDENTRESULT")
 public class StudentResult {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
