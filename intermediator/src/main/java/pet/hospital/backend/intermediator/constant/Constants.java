53968b8c7ad3195337c779f87a33fed5adb3fb7c
pikapikapikaori <pikapikapi_kaori@icloud.com>
2023-03-23 00:31:06 +0800

    refactor: 重写文件相关操作方法，使参数与返回值路径更纯粹

--- intermediator/src/main/java/pet/hospital/backend/intermediator/constant/Constants.java
+++ intermediator/src/main/java/pet/hospital/backend/intermediator/constant/Constants.java
@@ -1,8 +1,8 @@
 /*
  * @Author: pikapikapikaori pikapikapi_kaori@icloud.com
  * @Date: 2023-03-15 23:36:24
- * @LastEditors: pikapikapi pikapikapi_kaori@icloud.com
- * @LastEditTime: 2023-03-22 15:36:55
+ * @LastEditors: pikapikapikaori pikapikapi_kaori@icloud.com
+ * @LastEditTime: 2023-03-22 23:59:35
  * @FilePath: /virtualPetHospital-backend/intermediator/src/main/java/pet/hospital/backend/intermediator/constant/Constants.java
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -29,6 +29,8 @@ public class Constants {
             Arrays.asList("asx", "asf", "mpg", "wmv", "3gp", "mp4", "mov", "avi", "flv", "rmvb");
 
     public static String filePath = "filePath";
+
+    public static String filePathList = "filePathList";
 
     public static String expectedFormat = "expectedFormat";
 
