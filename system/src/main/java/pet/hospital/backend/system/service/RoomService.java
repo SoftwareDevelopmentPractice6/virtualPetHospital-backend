47cdfc8fd48c16298888ce87eaf02e701808570b
pikapikapikaori <pikapikapi_kaori@icloud.com>
2023-03-26 01:48:54 +0800

    fix: 修正可能的缺陷

--- system/src/main/java/pet/hospital/backend/system/service/RoomService.java
+++ system/src/main/java/pet/hospital/backend/system/service/RoomService.java
@@ -2,7 +2,7 @@
  * @Author: dafenqi-11 diaozehao@163.com
  * @Date: 2023-03-22 14:27:24
  * @LastEditors: pikapikapikaori pikapikapi_kaori@icloud.com
- * @LastEditTime: 2023-03-25 16:26:35
+ * @LastEditTime: 2023-03-26 01:44:34
  * @FilePath: \virtualPetHospital-backend\system\src\main\java\pet\hospital\backend\system\service\RoomService.java
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -100,6 +100,7 @@ public class RoomService {
                                     Objects.equals(feature.getFeatureRoom().getRoomName(), previousRoomName))
                             .forEach(feature -> {
                                 feature.setFeatureRoom(updatedRoom);
+                                featureRepository.saveAndFlush(feature);
                             });
 
                     roomRepository.deleteById(previousRoomName);
