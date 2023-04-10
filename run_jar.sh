cd847b04ac5263651a7c4250099d2e9f5dddd70c
pikapikapikaori <pikapikapi_kaori@icloud.com>
2023-04-10 19:57:35 +0800

    config: 修改脚本

--- run_jar.sh
+++ run_jar.sh
@@ -1,13 +1,50 @@
 #!/bin/sh
 
+export eureka_prod_log=logs/prod/eureka.log
+export login_prod_log=logs/prod/login.log
+export system_prod_log=logs/prod/system.log
+export medicalRecordManagement_prod_log=logs/prod/medicalRecordManagement.log
+export exam_prod_log=logs/prod/exam.log
+export intermediator_prod_log=logs/prod/intermediator.log
+
 startProd(){
     echo "--------------开始启动--------------"
-    (java -jar eureka.jar) &
-    (java -jar login.jar) &
-    (java -jar system.jar) &
-    (java -jar medicalRecordManagement.jar) &
-    (java -jar exam.jar) &
-    (java -jar intermediator.jar)
+    mkdir -p logs/prod;
+    echo "各模块运行记录可以在如下文件内查看："
+    echo "eureka: $eureka_prod_log"
+    echo "login: $login_prod_log"
+    echo "system: $system_prod_log"
+    echo "medicalRecordManagement: $medicalRecordManagement_prod_log"
+    echo "exam: $exam_prod_log"
+    echo "intermediator: $intermediator_prod_log"
+    echo "-----------------------------------"
+    (java -jar eureka.jar >> $eureka_prod_log) &
+    (java -jar login.jar >> $login_prod_log) &
+    (java -jar system.jar >> $system_prod_log) &
+    (java -jar medicalRecordManagement.jar >> $medicalRecordManagement_prod_log) &
+    (java -jar exam.jar >> $exam_prod_log) &
+    (java -jar intermediator.jar >> $intermediator_prod_log) &
+    (
+        echo "启动中......"
+        timer=0
+        while(( $timer<=100 ))
+        do
+            printf "$timer %%\r"
+            sleep 1
+            timer=`expr $timer + 2`
+        done
+        echo "启动完成"
+        echo "-----------------------------------"
+        echo "各模块运行地址："
+        echo "eureka: http://localhost:8085"
+        echo "login: http://localhost:8086"
+        echo "system: http://localhost:8087"
+        echo "medicalRecordManagement: http://localhost:8088"
+        echo "exam: http://localhost:8089"
+        echo "intermediator: http://localhost:8090"
+    ) &
+    wait
+    echo "项目被停止"
 }
 
 stopProd() {
