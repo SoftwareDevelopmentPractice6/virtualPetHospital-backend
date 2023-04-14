97157569da074f93f8c03607192164f275ee4e4a
pikapikapi <pikapikapi_kaori@icloud.com>
2023-04-14 08:00:50 +0800

    version: BUMP TO 1.0.1-SNAPSHOT

--- intermediator/src/main/java/pet/hospital/backend/intermediator/IntermediatorApplication.java
+++ intermediator/src/main/java/pet/hospital/backend/intermediator/IntermediatorApplication.java
@@ -1,8 +1,8 @@
 /*
  * @Author: pikapikapikaori pikapikapi_kaori@icloud.com
  * @Date: 2023-03-15 23:30:24
- * @LastEditors: pikapikapikaori pikapikapi_kaori@icloud.com
- * @LastEditTime: 2023-04-06 17:57:15
+ * @LastEditors: pikapikapi pikapikapi_kaori@icloud.com
+ * @LastEditTime: 2023-04-14 07:58:16
  * @FilePath: /virtualPetHospital-backend/intermediator/src/main/java/pet/hospital/backend/intermediator/IntermediatorApplication.java
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -22,7 +22,7 @@ import org.springframework.web.client.RestTemplate;
         info =
                 @Info(
                         title = "中间层模块",
-                        version = "1.0.0-SNAPSHOT",
+                        version = "1.0.1-SNAPSHOT",
                         description =
                                 "虚拟宠物医院API文档，前端用。字段默认值：int为-1，String为'This field is currently Empty.'，Date为null，应该会表现为空字符串。"))
 public class IntermediatorApplication {
