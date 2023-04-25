8542c490d2fc4574d2c33b1ccfeb52c0f1220deb
pikapikapikaori <pikapikapi_kaori@icloud.com>
2023-04-25 18:37:46 +0800

    fix: 修复fastjson序列化时循环引用问题

---
+++ exam/src/main/java/pet/hospital/backend/config/FastJsonDefaultInitConfig.java
@@ -0,0 +1,23 @@
+/*
+ * @Author: pikapikapikaori pikapikapi_kaori@icloud.com
+ * @Date: 2023-04-25 18:36:18
+ * @LastEditors: pikapikapikaori pikapikapi_kaori@icloud.com
+ * @LastEditTime: 2023-04-25 18:36:45
+ * @FilePath: /virtualPetHospital-backend/exam/src/main/java/pet/hospital/backend/config/FastJsonDefaultInitConfig.java
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+package pet.hospital.backend.config;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+@Component
+public class FastJsonDefaultInitConfig {
+
+    @PostConstruct
+    public void postConstruct() {
+        JSON.DEFAULT_GENERATE_FEATURE |= SerializerFeature.DisableCircularReferenceDetect.getMask();
+    }
+}
